c2952dea4413c9bc944d21b574d3a2be67f970ec
appledocta <maccordoba1980@gmail.com>
2017-04-13 20:05:15 -0300

    conexion

--- Pucura/CapaDA/Conexion.vb
+++ Pucura/CapaDA/Conexion.vb
@@ -1,27 +1,29 @@
-﻿Imports System.Data.Sql
-Imports System.Data.SqlClient
-Imports System.Configuration
+﻿Imports System.Data.SqlClient
 
-Public Class ConexionDA
-    Public Shared conn As New SqlConnection
-    Public Shared Function Conectado()
+Public Class conexion
+
+    Protected cnn As New SqlConnection
+    Public id_usuario As Integer
+
+    'Funcion para conectarme a la BD
+    Protected Function conectado()
+
         Try
-            '''''con = New SqlConnection("Server=(local);Database=Proyecto;Trusted_Connection=True")
-            ''''' conn = New SqlConnection("Data Source=LJBLANCO\SQLSERVER2008;Initial Catalog=LocalCuadros;Integrated Security=True")            'Lidio
-            ''''''conn = New SqlConnection("Data source=DESKTOP-RA5UK2L\SQLSERVER;initial catalog=LocalCuadros;integrated security=true")       'Cristian
-            conn = New SqlConnection("Data source=(local);initial catalog=Pucara;integrated security=true") 'Sancho
-            conn.Open()
+            cnn = New SqlConnection("data source=DESKTOP-RA5UK2L\SQLSERVER;initial catalog= Pucara; integrated security = true")
+            cnn.Open()
             Return True
         Catch ex As Exception
             MsgBox(ex.Message)
             Return False
         End Try
+
     End Function
 
-    Public Shared Function Desconectado()
+    'Funcion para desconectarme de la BD
+    Protected Function desconectado()
         Try
-            If conn.State = ConnectionState.Open Then
-                conn.Close()
+            If cnn.State = ConnectionState.Open Then
+                cnn.Close()
                 Return True
             Else
                 Return False
@@ -29,22 +31,7 @@ Public Class ConexionDA
         Catch ex As Exception
             MsgBox(ex.Message)
             Return False
+
         End Try
     End Function
-
-    'Lidio
-    Public Function abrir() As SqlConnection
-        Dim con As String
-        Dim scon As SqlConnection
-        'con = "Data Source=PUESTO155;Initial Catalog=EsquemaFisico;Integrated"
-        'con = "Data Source=LJBLANCO\SQLSERVER2008;Initial Catalog=LocalCuadros;Integrated Security=True"            'Lidio
-        'con = "Data Source=DESKTOP-RA5UK2L\SQLSERVER;Initial Catalog=LocalCuadros;Integrated Security=True"       'Cristian
-
-        con = "Data source=(local);initial catalog=Pucara;integrated security=true" 'Sancho
-        scon = New SqlConnection(con)
-        scon.Open()
-        Return scon
-    End Function
-
 End Class
-
